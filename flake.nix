ee480e74bc3015bc46a71fdef384c9cf6d7ecbcc
Teo Lemane <teo.lemane@gmail.com>
2023-03-08 23:59:48 +0100

    flake: remove osx

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     };
   };
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [
-    "x86_64-linux" "x86_64-darwin"
+    "x86_64-linux"
   ] (system:
     let
       pkgs = import nixpkgs {
